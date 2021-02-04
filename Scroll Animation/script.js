9d642a0e1cf2852bde74b60f24965de8100927ea
Murtaza Vasi <Murtaza-Vasi>
2021-02-04 18:52:25 +0530

    Completed Project Files

---
+++ Scroll Animation/script.js
@@ -0,0 +1,16 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        if (box.getBoundingClientRect().top < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+
+}
